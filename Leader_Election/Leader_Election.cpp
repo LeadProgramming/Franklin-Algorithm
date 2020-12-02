4c87fe78407524428d43ce74a9cd9428df699d47
David Nguyen <vietcloudx@csu.fullerton.edu>
2020-12-02 05:43:00 -0800

    Comm_world

--- Leader_Election/Leader_Election.cpp
+++ Leader_Election/Leader_Election.cpp
@@ -11,33 +11,49 @@ using a fixed number of processors.
 #include <stdlib.h> /* rand */
 #include <string> /* stoi */
 #include <vector>
+#include "./CPSC-131-Data-Structures-code/CircularDummyNodeDLinkedList.hxx";
+#include <iterator>
 using namespace std;
 
-vector<int> randGenerator(size_t size) {
-	vector<int> result;
+struct Process {
+	int index;
+	int value;
+};
+DLinkedList<Process> randGenerator(size_t size) {
+	DLinkedList<Process> generated;
 	for (size_t i = 0; i < size; i++) {
-		int rando = rand() % 1000 + 1;
-		result.push_back(rando);
+		Process elem;
+		elem.index = i;
+		elem.value = rand() % 1000 + 1;
+		generated.append(elem);
 	}
-	return result;
+	return generated;
 }
-void displayList(vector<int> x) {
+void displayList(DLinkedList<Process> x) {
 	cout << "Process 0: ";
-	for (int i : x) {
-		cout << i << " ";
+	
+	for (DLinkedList<Process>::Iterator i = x.begin(); i != x.end();i++) {
+		cout << i->value << " ";
 	}
 	cout << endl;
 }
-
 int main(int argc, char* argv[])
 {
 	srand(time(NULL));
 	size_t len = stoi(argv[1]);
 
-	vector<int> datalist = randGenerator(len);
+	MPI_Init(&argc, &argv);
+	MPI_Datatype node_type;
+	int lengths[2] = { 1, 1 };
+	const MPI_Aint displacements[2] = { 0, sizeof(int) };
+	MPI_Datatype types[2] = { MPI_INT, MPI_INT };
+	MPI_Type_create_struct(2, lengths, displacements, types, &node_type);
+	MPI_Type_commit(&node_type);
+
+	DLinkedList<Process> datalist = randGenerator(len);
+	DLinkedList<Process>::Iterator curr = datalist.begin();
 	int round = 0;
 	while (datalist.size() > 1) {
-		MPI_Init(&argc, &argv);
 		int rank;
 		int size;
 		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -45,81 +61,82 @@ int main(int argc, char* argv[])
 
 		MPI_Status status;
 		MPI_Request req;
-		//MPI_Request* initialSend = new MPI_Request[size*2];
 		int count = 0;
-		int tmp = 0;
+		struct Process tmp;
+		tmp.index = 0;
+		tmp.value = 0;
 		//consumer	
 		if (rank == 0) {
 			displayList(datalist);
+			for (curr = datalist.begin(); curr != datalist.end(); curr++) {
+				//cout << curr->index << endl;
+				MPI_Send(&*curr, 1, node_type, curr->index + 1, 0, MPI_COMM_WORLD);
+			}
+			//send first
+			MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Barrier(MPI_COMM_WORLD);
 			//raymond algorithm 
-			MPI_Ssend(&count, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-			MPI_Recv(&count, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &status);
+			MPI_Ssend(&datalist.front(), datalist.size(), node_type, datalist.front().index + 1, 0, MPI_COMM_WORLD);
+			MPI_Recv(&datalist.front(), datalist.size(), node_type, datalist.back().index + 1, 0, MPI_COMM_WORLD, &status);
 			MPI_Barrier(MPI_COMM_WORLD);
-			if (status.MPI_SOURCE == size - 1) {
+			if (status.MPI_SOURCE) {
 				cout << count << endl;
-				vector<int> tmpList;
+				DLinkedList<Process> tmpList;
 				for (int i = 0; i < count; i++) {
-					MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-					cout << tmp << " ";
-					tmpList.push_back(tmp);
+					MPI_Recv(&tmp, 1, node_type, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+					cout << tmp.value << " ";
+					tmpList.append(tmp);
 				}
 				cout << endl;
-				datalist = tmpList;
+				//datalist = tmpList;
 			}
 			round++;
 		}
 		//producer
 		else {
-			tmp = datalist[rank - 1];
-			cout << "Rank: " << rank << endl;
-			vector<int> comp;
-			comp.push_back(datalist[rank - 1]);
+			//we need to send first
 			MPI_Barrier(MPI_COMM_WORLD);
-			int left = rank - 1;
-			int right = rank + 1;
-			if (rank == 1) {
-				left = size - 1;
-				right = rank + 1;
-			}
-			else if (rank == size - 1) {
-				left = 1;
-				right = rank - 1;
-			}
-			MPI_Send(&datalist[rank - 1], 1, MPI_INT, left, 0, MPI_COMM_WORLD);
-			MPI_Send(&datalist[rank - 1], 1, MPI_INT, right, 0, MPI_COMM_WORLD);
+			MPI_Recv(&*curr, 1, node_type, 0, 0, MPI_COMM_WORLD, &status);
+			cout << curr->value << endl;
+			cout << datalist.front().value << endl;
+			tmp = *curr;
+			vector<Process> comp;
+			comp.push_back(*curr);
+			
+
 			MPI_Barrier(MPI_COMM_WORLD);
-			MPI_Irecv(&datalist[rank - 1], 1, MPI_INT, left, 0, MPI_COMM_WORLD, &req);
-			comp.push_back(datalist[rank - 1]);
+			MPI_Send(&*curr, 1, node_type, curr.prev(1)->index+1, 0, MPI_COMM_WORLD);
+			MPI_Send(&*curr, 1, node_type, curr.next(1)->index+1, 0, MPI_COMM_WORLD);
+			MPI_Barrier(MPI_COMM_WORLD);
+			MPI_Irecv(&*curr, 1, node_type, curr.prev(1)->index+1, 0, MPI_COMM_WORLD, &req);
+			comp.push_back(*curr);
 			MPI_Wait(&req, &status);
-			MPI_Irecv(&datalist[rank - 1], 1, MPI_INT, right, 0, MPI_COMM_WORLD, &req);
-			comp.push_back(datalist[rank - 1]);
+			MPI_Irecv(&*curr, 1, node_type, curr.next(1)->index+1, 0, MPI_COMM_WORLD, &req);
+			comp.push_back(*curr);
 			MPI_Wait(&req, &status);
 			MPI_Barrier(MPI_COMM_WORLD);
-			//cout << "Rank: " << rank << " ";
-			//for (int i : comp) {
-			//	cout << i << " ";
+			MPI_Recv(&datalist.front(), 1, node_type, curr.prev(1)->index+1 , 0, MPI_COMM_WORLD, &status);
+			if (status.MPI_ERROR) {
+				cout << __LINE__ << endl;
+			}
+			////we only want the heavy influencers.
+			//if (comp[0].value > comp[1].value && comp[0].value > comp[2].value) {
+			//	count++;
 			//}
-			//cout << endl;
-			MPI_Recv(&count, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &status);
-			//we only want the heavy influencers.
-			if (comp[0] > comp[1] && comp[0] > comp[2]) {
-				count++;
-			}
-			if (rank == size - 1) {
-				MPI_Ssend(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-			}
-			else {
-				MPI_Ssend(&count, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
-			}
-			MPI_Barrier(MPI_COMM_WORLD);
-			if (comp[0] > comp[1] && comp[0] > comp[2]) {
-				tmp = comp[0];
-				cout << "Provider Sending: " << rank << " : " << tmp << endl;
-				MPI_Send(&tmp, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-			}
+			//if (rank == size - 1) {
+			//	MPI_Ssend(datalist.data(), 1, node_type, datalist.begin()->index+1, 0, MPI_COMM_WORLD);
+			//}
+			//else {
+			//	MPI_Ssend(datalist.data(), 1, node_type, next(curr,1)->index+1, 0, MPI_COMM_WORLD);
+			//}
+			//MPI_Barrier(MPI_COMM_WORLD);
+			//if (comp[0].value > comp[1].value && comp[0].value > comp[2].value) {
+			//	tmp = comp[0];
+			//	cout << "Provider Sending: " << rank << " : " << tmp.value << endl;
+			//	MPI_Send(&tmp, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			//}
 		}
 		MPI_Finalize();
 	}
